bf042120ad54e3f5b60ed4b690dd217441345f9a
Fabian Meier <reiem_naibaf@hotmail.com>
2020-01-15 22:04:38 +0100

    add logic for rotor

---
+++ src/enigma/rotor-box/rotor.test.ts
@@ -0,0 +1,93 @@
+import { Rotor } from './rotor';
+
+function stringToCharArray(str: string): string[] {
+  const chars = [];
+  for (let i = 0; i < str.length; i++) {
+    chars.push(str.charAt(i));
+  }
+  return chars;
+}
+
+function createRotorI(): Rotor {
+  return new Rotor(
+    stringToCharArray('ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+    stringToCharArray('EKMFLGDQVZNTOWYHXUSPAIBRCJ'),
+    'Q'
+  );
+}
+
+describe(Rotor.name, () => {
+  describe('encrypt', () => {
+    it('should return letter according to mapping', () => {
+      const rotor = createRotorI();
+
+      expect(rotor.encrypt('B')).toEqual('K');
+    });
+
+    it('should return letter according to mapping and position', () => {
+      const rotor = createRotorI();
+      rotor.setPosition('V');
+
+      expect(rotor.encrypt('B')).toEqual('B');
+    });
+  });
+
+  describe('encryptReverse', () => {
+    it('should return letter according to mapping', () => {
+      const rotor = createRotorI();
+
+      expect(rotor.encryptReverse('K')).toEqual('B');
+    });
+
+    it('should return letter according to mapping and position', () => {
+      const rotor = createRotorI();
+      rotor.setPosition('V');
+
+      expect(rotor.encryptReverse('B')).toEqual('B');
+    });
+  });
+
+  describe('setPosition', () => {
+    it('should set rotor position correctly', () => {
+      const rotor = createRotorI();
+
+      rotor.setPosition('C');
+
+      expect(rotor.positionLabel).toEqual('C');
+    });
+  });
+
+  describe('step', () => {
+    it('should move position one step further', () => {
+      const rotor = createRotorI();
+      rotor.setPosition('O');
+
+      rotor.step();
+
+      expect(rotor.positionLabel).toEqual('P');
+    });
+
+    it('should set position to first letter when alphabet ends', () => {
+      const rotor = createRotorI();
+      rotor.setPosition('Z');
+
+      rotor.step();
+
+      expect(rotor.positionLabel).toEqual('A');
+    });
+
+    it('should return true on turnover', () => {
+      const rotor = createRotorI();
+      rotor.setPosition('Q');
+
+      expect(rotor.step()).toBeTruthy();
+    });
+
+    it('should return false when no turnover happened', () => {
+      const rotor = createRotorI();
+      rotor.setPosition('N');
+
+      expect(rotor.step()).toBeFalsy();
+    });
+  });
+});
---
+++ src/enigma/rotor-box/rotor.ts
@@ -0,0 +1,58 @@
+export class Rotor {
+  private alphabetSize: number;
+
+  private alphabet: Map<string, number>;
+
+  private alphabetReverse: Map<number, string>;
+
+  private mapping: Map<number, string>;
+
+  private mappingReverse: Map<string, number>;
+
+  private turnover: number;
+
+  private position: number;
+
+  get positionLabel(): string {
+    return this.alphabetReverse.get(this.position)!;
+  }
+
+  constructor(alphabet: string[], mapping: string[], turnover: string) {
+    this.alphabetSize = alphabet.length;
+    this.position = 0;
+    this.turnover = alphabet.indexOf(turnover);
+
+    this.alphabet = new Map<string, number>();
+    this.alphabetReverse = new Map<number, string>();
+    this.mapping = new Map<number, string>();
+    this.mappingReverse = new Map<string, number>();
+    for (let i = 0; i < alphabet.length; i++) {
+      this.alphabet.set(alphabet[i], i);
+      this.alphabetReverse.set(i, alphabet[i]);
+      this.mapping.set(i, mapping[i]);
+      this.mappingReverse.set(mapping[i], i);
+    }
+  }
+
+  encrypt(letter: string): string {
+    const index = this.alphabet.get(letter)!;
+    const encryptedLetter = this.mapping.get((index + this.position) % this.alphabetSize)!;
+    return encryptedLetter;
+  }
+
+  encryptReverse(letter: string): string {
+    const index = (this.mappingReverse.get(letter)! - this.position) % this.alphabetSize;
+    const encryptedLetter = this.alphabetReverse.get(index)!;
+    return encryptedLetter;
+  }
+
+  setPosition(letter: string) {
+    this.position = this.alphabet.get(letter)!;
+  }
+
+  step(): boolean {
+    const turnover = this.position === this.turnover;
+    this.position = (this.position + 1) % this.alphabetSize;
+    return turnover;
+  }
+}
