3519dcd26a43d22d2482e894753efb6c6568443a
Fabian Meier <reiem_naibaf@hotmail.com>
2020-01-18 21:33:33 +0100

    add type to reflector

--- src/enigma/rotor-box/reflector.ts
+++ src/enigma/rotor-box/reflector.ts
@@ -3,8 +3,12 @@ import { Rotor } from './rotor';
 export class Reflector {
   private rotor: Rotor;
 
-  constructor(alphabet: string[], mapping: string[]) {
-    this.rotor = new Rotor(alphabet, mapping, alphabet[0]);
+  get type(): string {
+    return this.typeDescription;
+  }
+
+  constructor(private typeDescription: string, alphabet: string[], mapping: string[]) {
+    this.rotor = new Rotor(typeDescription, alphabet, mapping, alphabet[0]);
   }
 
   encrypt(letter: string): string {
